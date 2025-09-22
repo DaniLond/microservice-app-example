67d2ef597a3bac0d25f4e54110342e4b07ebf558
isabella huila cerom <isahc221004@gmail.com>
2025-09-22 16:19:33 -0500

    feat: add circuit breaker to controller

--- users-api/src/main/java/com/elgris/usersapi/api/UsersController.java
+++ users-api/src/main/java/com/elgris/usersapi/api/UsersController.java
@@ -1,32 +1,45 @@
 package com.elgris.usersapi.api;
 
-import com.elgris.usersapi.models.User;
-import com.elgris.usersapi.repository.UserRepository;
-import io.jsonwebtoken.Claims;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
 import java.util.LinkedList;
 import java.util.List;
 
-@RestController()
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.elgris.usersapi.models.User;
+import com.elgris.usersapi.models.UserRole;
+import com.elgris.usersapi.repository.UserRepository;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.jsonwebtoken.Claims;
+@RestController
 @RequestMapping("/users")
 public class UsersController {
 
     @Autowired
     private UserRepository userRepository;
 
-
+    @CircuitBreaker(name = "getUsersCB", fallbackMethod = "getUsersFallback")
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public List<User> getUsers() {
         List<User> response = new LinkedList<>();
         userRepository.findAll().forEach(response::add);
 
-        return response;
+        throw new RuntimeException("Simulación: La base de datos no está disponible.");
     }
 
+    public List<User> getUsersFallback(Throwable t) {
+        System.out.println("Fallback getUsers triggered: " + t.getMessage());
+        return new LinkedList<>(); // lista vacía
+    }
+
+    @CircuitBreaker(name = "getUserCB", fallbackMethod = "getUserFallback")
     @RequestMapping(value = "/{username}",  method = RequestMethod.GET)
     public User getUser(HttpServletRequest request, @PathVariable("username") String username) {
 
@@ -44,4 +57,13 @@ public class UsersController {
         return userRepository.findOneByUsername(username);
     }
 
+    public User getUserFallback(HttpServletRequest request, String username, Throwable t) {
+        User fallbackUser = new User();
+        fallbackUser.setUsername("unknown");
+        fallbackUser.setFirstname("unknown");
+        fallbackUser.setLastname("unknown");
+        fallbackUser.setRole(UserRole.USER);
+        return fallbackUser;
+    }
 }
+
